6533635d56c01151af9ce4b487e502d9a708a80f
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-09 18:30:44 +0530

    database-connection

---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+async function dbConnect() {
+    if (mongoose.connection.readyState) {
+        console.log('Already connected to database');
+        return;
+    }
+
+    if (!process.env.MONGO_URI) {
+        console.log('MongoDB uri not geted');
+        return 
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection error:", error);
+    }
+}
+
+module.exports = dbConnect;
